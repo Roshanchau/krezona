bc7f3f8dc3eb6ab3fd87f0b03778066a08ee088c
Roshanchau <roshanchau001@gmail.com>
2025-07-11 15:44:44 +0545

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,10 @@ import NavLinks from "./NavLinks";
 export const revalidate=0;
 
 const Navbar = () => {
+  const handleClick = () => {
+    window.location.hash = "#Contact";
+  }
+
   return (
     <nav>
         <div className=" px-4 hidden sm:px-6 lg:px-8 md:flex flex-row h-16 items-center justify-around text-neutral-50 ">
@@ -12,11 +16,10 @@ const Navbar = () => {
             <img src="/logo/GAM.png" alt="logo" className=""/>
           </div>
           <NavLinks label={ ['About' , 'Games' ,'Services' , 'Events'  ,'Blog' ]} />
-          <button className="flex items-center justify-center border-2 text-sm border-neutral-400 border-opacity-60 px-4 py-2 rounded-lg">CONTACT US</button>
+          <button onClick={handleClick} className="flex items-center justify-center border-2 text-sm border-neutral-400 border-opacity-60 px-4 py-2 rounded-lg">CONTACT US</button>
         </div>
     </nav>  
   );  
 };
 
 export default Navbar;
-  
